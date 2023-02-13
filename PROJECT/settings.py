36c9a96223922b038afa70aaefe1d70a1aeb2ff0
prosper <edwardprosper001@gmail.com>
2023-02-13 13:14:12 +0100

    'now'

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=7vbsvg6eic8@p0c!cd064@(9ra#6nb0lyl6@+o-0fu-il=h+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
